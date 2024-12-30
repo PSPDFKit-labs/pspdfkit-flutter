3d1c1fb9864777fb2d196de3dd8562f77bb93904
Julius Kato Mutumba <mutumbakato@gmail.com>
2024-12-31 00:52:08 +0300

    Update FlutterPdfUiFragment.kt

--- android/src/main/java/com/pspdfkit/flutter/pspdfkit/FlutterPdfUiFragment.kt
+++ android/src/main/java/com/pspdfkit/flutter/pspdfkit/FlutterPdfUiFragment.kt
@@ -16,14 +16,61 @@ package com.pspdfkit.flutter.pspdfkit
 ///  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
 ///  This notice may not be removed from this file.
 ///
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.pspdfkit.document.PdfDocument
 import com.pspdfkit.ui.PdfUiFragment
+import com.pspdfkit.ui.toolbar.AnnotationEditingToolbar
+import com.pspdfkit.ui.toolbar.ContextualToolbar
+import com.pspdfkit.ui.toolbar.ContextualToolbarMenuItem
+import com.pspdfkit.ui.toolbar.ToolbarCoordinatorLayout
 
-class FlutterPdfUiFragment : PdfUiFragment() {
+class FlutterPdfUiFragment : PdfUiFragment(),  ToolbarCoordinatorLayout.OnContextualToolbarLifecycleListener {
+
+    private lateinit var customTextSelectionAction: ContextualToolbarMenuItem
+    private val id = View.generateViewId()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Create a custom menu item that will be shown inside the text selection toolbar.
+        customTextSelectionAction = ContextualToolbarMenuItem.createSingleTextItem(
+            requireContext(),
+            id,
+            ContextualToolbarMenuItem.Position.END
+        )
+    }
 
     override fun onDocumentLoaded(document: PdfDocument) {
         super.onDocumentLoaded(document)
       // Notify the Flutter PSPDFKit plugin that the document has been loaded.
         EventDispatcher.getInstance().notifyDocumentLoaded(document)
+    }
+
+    override fun onPrepareContextualToolbar(toolbar: ContextualToolbar<*>) {
+        if (toolbar is AnnotationEditingToolbar) {
+            val menuItems = toolbar.getMenuItems()
+            if (!menuItems.contains(customTextSelectionAction)) {
+                menuItems.add(customTextSelectionAction)
+                toolbar.setMenuItems(menuItems)
+            }
+        }
+    }
+
+    override fun onDisplayContextualToolbar(toolbar: ContextualToolbar<*>) {
+        toolbar.setOnMenuItemClickListener { _, menuItem ->
+            var handled = false
+            if (menuItem.id == id) {
+                handled = true
+                Toast.makeText(
+                    requireContext(), "Custom action triggered!", Toast.LENGTH_SHORT
+                ).show()
+            }
+            handled
+        }
+    }
+
+    override fun onRemoveContextualToolbar(toolbar: ContextualToolbar<*>) {
+        toolbar.setOnMenuItemClickListener(null)
     }
 }
