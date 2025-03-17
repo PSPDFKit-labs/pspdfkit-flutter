e8ed6228993cbd628684c5eb115c01a13a570bfe
Julius Kato Mutumba <mutumbakato@gmail.com>
2025-03-17 12:38:53 +0300

    custom stamps - preset

--- android/src/main/java/com/pspdfkit/flutter/pspdfkit/FlutterPdfUiFragment.kt
+++ android/src/main/java/com/pspdfkit/flutter/pspdfkit/FlutterPdfUiFragment.kt
@@ -16,6 +16,11 @@ package com.pspdfkit.flutter.pspdfkit
 ///  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
 ///  This notice may not be removed from this file.
 ///
+import android.os.Bundle
+import android.view.View
+import com.pspdfkit.annotations.AnnotationType
+import com.pspdfkit.annotations.configuration.StampAnnotationConfiguration
+import com.pspdfkit.annotations.stamps.StampPickerItem
 import com.pspdfkit.document.PdfDocument
 import com.pspdfkit.ui.PdfUiFragment
 
@@ -25,5 +30,18 @@ class FlutterPdfUiFragment : PdfUiFragment() {
         super.onDocumentLoaded(document)
       // Notify the Flutter PSPDFKit plugin that the document has been loaded.
         EventDispatcher.getInstance().notifyDocumentLoaded(document)
+        val items = listOf(
+            StampPickerItem.fromTitle(requireContext(), "Great!").build(),
+            StampPickerItem.fromTitle(requireContext(), "Stamp!").build(),
+            StampPickerItem.fromTitle(requireContext(), "Like").build()
+        )
+        // Available stamps can be configured through the `PdfFragment`.
+        requirePdfFragment().annotationConfiguration.put(
+            AnnotationType.STAMP,
+            StampAnnotationConfiguration.builder(requireContext())
+                // Here you set the list of stamp picker items that are going to be available in the stamp picker.
+                .setAvailableStampPickerItems(items)
+                .build()
+        )
     }
 }
